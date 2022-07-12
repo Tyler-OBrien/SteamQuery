c654d5f3a25a3b4f632a14609f54c91b6924def0
RealChaika <94197210+RealChaika@users.noreply.github.com>
2022-07-11 21:52:50 -0400

    A2S_Rules:  Some servers will bypass replying with a challenge, and instead will immediately send a full rules response.

--- SteamQueryNet/SteamQueryNet/ServerQuery.cs
+++ SteamQueryNet/SteamQueryNet/ServerQuery.cs
@@ -254,7 +254,7 @@ public class ServerQuery : IServerQuery, IDisposable
 
         var tryGetHeader = response.Skip(4).First();
 
-        if (tryGetHeader != PacketHeaders.A2S_RULES_S2C_CHALLENGE)
+        if (tryGetHeader != PacketHeaders.A2S_RULES_S2C_CHALLENGE && tryGetHeader != PacketHeaders.A2S_RULES_RESPONSE)
         {
 #if DEBUG
             Console.WriteLine(
@@ -263,16 +263,19 @@ public class ServerQuery : IServerQuery, IDisposable
             return null;
         }
 
-        var challenge = BitConverter.ToInt32(
-            response.Skip(DataResolutionUtils.RESPONSE_CODE_INDEX).Take(sizeof(int)).ToArray(),
-            0);
+        if (tryGetHeader == PacketHeaders.A2S_RULES_S2C_CHALLENGE)
+        {
+            var challenge = BitConverter.ToInt32(
+                response.Skip(DataResolutionUtils.RESPONSE_CODE_INDEX).Take(sizeof(int)).ToArray(),
+                0);
 
-        response = await SendRequestAsync(
-            RequestHelpers.PrepareAS2_GENERIC_Request(PacketHeaders.A2S_RULES, challenge),
-            cancellationToken);
+            response = await SendRequestAsync(
+                RequestHelpers.PrepareAS2_GENERIC_Request(PacketHeaders.A2S_RULES, challenge),
+                cancellationToken);
 
 
-        tryGetHeader = response.Skip(4).First();
+            tryGetHeader = response.Skip(4).First();
+        }
 
 
         if (tryGetHeader != PacketHeaders.A2S_RULES_RESPONSE)
